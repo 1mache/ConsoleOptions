9818ea35ce07b18f8a8a41e68c0f9589cbd68854
Imache <miktor4925@gmail.com>
2022-10-20 19:57:04 +0300

    Add ParamAttribute custom attribute for console parameters

---
+++ ParamAttribute.cs
@@ -0,0 +1,10 @@
+class ParamAttribute : Attribute
+{
+    public bool Optional { get; }
+    public string Description {get;}
+    public ParamAttribute(bool optional, string description)
+    {
+        Optional = optional;
+        Description = description;
+    }
+}
