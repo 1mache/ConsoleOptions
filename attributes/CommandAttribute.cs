3fda11437054b14f43f0cb2463290c7fbd2f3e74
Imache <miktor4925@gmail.com>
2022-11-20 03:32:01 +0200

    Removed dash check from CommandAttribute constructor

--- attributes/CommandAttribute.cs
+++ attributes/CommandAttribute.cs
@@ -6,10 +6,7 @@ namespace ConsoleOptions
         public string Name { get;}
         public string Description{ get;}
         public CommandAttribute(string name, string description)
-        {
-            if(name[0].Equals('-'))
-                throw new OptionException("Name of command must not have a - as first symbool");
-            
+        {   
             Name = name;
             Description = description;
         }
